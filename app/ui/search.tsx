5d0379e4db32a144608ac06bbc30b92602ebf760
SANGANA SWARNA <ucs20339@rmd.ac.in>
2024-02-01 10:55:40 +0530

    add commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,7 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({ placeholder }: { placeholder: string })  {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
